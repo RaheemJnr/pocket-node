9e7840d5bbfd890927ffc80b114ddd32fde25efe
raheemjnr <mumedian6@gmail.com>
2026-04-29 19:02:41 +0100

    chore(release): retarget version bump to 1.5.1
    More changes are queued before 1.6.0; this release ships the
    user-education work as a patch instead.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 7
-        versionName = "1.6.0"
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
