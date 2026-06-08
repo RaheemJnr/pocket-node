19c57654dc8353ad37e25f87a6c131ef7322575e
raheemjnr <mumedian6@gmail.com>
2026-06-08 18:44:16 +0100

    chore(release): bump versionCode 12->13, versionName 1.7.1->1.7.2 (#289 chunk 7)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rjnr.pocketnode"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.7.1"
+        versionCode = 13
+        versionName = "1.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
